0a0cffd17b919f4465c246f1408ab37fd8fc0100
Kevin Eddie <keddie024@gmail.com>
2021-06-06 18:26:08 -0400

    added code for intern creation

--- index.js
+++ index.js
@@ -175,6 +175,90 @@ const createEngineer = () => {
     });
 }
 
+const createIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the intern's name.",
+            validate: data => {
+                if(data !== ""){
+                    return true
+                } 
+                return "Name not detected. Please enter a name."
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the intern's ID.",
+            validate: data => {
+                if(data !== ""){
+                    return true
+                } 
+                return "ID not detected. Please enter an ID."
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter the intern's email.",
+            validate: data => {
+                if(data.match(/\S+@\S+\.\S+/)) {
+                    return true
+                }
+                return "Please enter a valid email address."
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school did the intern attend?",
+            validate: data => {
+                if(data !== ""){
+                    return true
+                } 
+                return "Please enter a school."
+            }
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Would you like to add another team member?",
+            choices: ['Manager', 'Engineer', 'Intern', 'Team Complete. Exit Program.'],
+        },
+
+    ]).then((answers) => {
+        const intern = new Intern(
+            answers.name, 
+            answers.id,
+            answers.email,
+            answers.school
+            );
+            team.push(intern)
+            let Continue = answers.teamMember !== 'Team Complete. Exit Program.';
+            let Type = answers.teamMember;
+
+            if (!Continue) {
+                writeHTML();
+                return;
+            }
+
+            while (Continue) {
+                if (Type === 'Engineer') {
+                createEngineer();
+            }
+                else if (Type === 'Intern') {
+                createIntern();
+            }
+                else if (Type === 'Manager'){
+                createManager();
+            }
+            return
+        }
+    });
+}
+
 createManager();
 
 const writeHTML = () => {
