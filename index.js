032b59b781f76db54e0aff9dd43026417283de7a
Kevin Eddie <keddie024@gmail.com>
2021-06-06 18:12:42 -0400

    added code for engineer creation

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const createManager = () => {
         {
             type: "input",
             name: "id",
-            message: "Please enter the team manager's id.",
+            message: "Please enter the team manager's ID.",
             validate: data => {
                 if (data !== "") {
                     return true
@@ -68,6 +68,90 @@ const createManager = () => {
             answers.officeNumber
         );
         team.push(manager)
+        let Continue = answers.teamMember !== 'Team Complete. Exit Program.';
+        let Type = answers.teamMember;
+
+        if (!Continue) {
+            writeHTML();
+            return;
+        }
+
+        while (Continue) {
+            if (Type === 'Engineer') {
+                createEngineer();
+            }
+            else if (Type === 'Intern') {
+                createIntern();
+            }
+            else if (Type === 'Manager') {
+                createManager();
+            }
+            return
+        }
+    });
+}
+
+const createEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the engineer's name.",
+            validate: data => {
+                if (data !== "") {
+                    return true
+                }
+                return "Name not detected. Please enter a name."
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the engineer's ID.",
+            validate: data => {
+                if (data !== "") {
+                    return true
+                }
+                return "ID not detected. Please enter an ID."
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter the engineer's email.",
+            validate: data => {
+                if (data.match(/\S+@\S+\.\S+/)) {
+                    return true
+                }
+                return "Please enter a valid email address."
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter the engineer's GitHub username.",
+            validate: data => {
+                if (data !== "") {
+                    return true
+                }
+                return "No username detected. Please enter a username."
+            }
+        },
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Would you like to add another team member?",
+            choices: ['Manager', 'Engineer', 'Intern', 'Team Complete. Exit Program.'],
+        },
+
+    ]).then((answers) => {
+        const engineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github
+        );
+        team.push(engineer)
         let Continue = answers.teamMember !== 'Team Complete. Exit Program.';
         let Type = answers.teamMember;
 
