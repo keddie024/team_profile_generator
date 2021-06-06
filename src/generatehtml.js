67eaf1edf033fec2fc793ffd4430711dc1a5e500
Kevin Eddie <keddie024@gmail.com>
2021-06-06 16:38:07 -0400

    base html created

---
+++ src/generatehtml.js
@@ -0,0 +1,20 @@
+let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>The Team</title>
+</head>
+<body>
+    
+    <header>
+        <div class="jumbotron jumbotron-fluid custom-jumbotron">
+            <div class="container">
+                <h1 class= "display-4">Team Profile Generator</h1>
+            </div>
+        </div>
+    </header>
+    <main>`
